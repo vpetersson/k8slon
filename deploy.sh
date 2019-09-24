c80b50c45f2a801d63c1e68fe4d1161609ad5a73
Viktor Petersson <vpetersson@wireload.net>
2019-09-24 11:03:43 +0100

    Fixes newlines

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,7 @@ IFS=$'\n\t'
 
 GITHASH="git-$(git rev-parse --short HEAD)"
 
-kubectl -n k8slon --record deployment.apps/k8slon-deployment set image deployment.v1.apps/k8slon-deployment k8slon="vpetersson/k8slon:$GITHASH"
+kubectl -n k8slon \
+    --record deployment.apps/k8slon-deployment \
+    set image deployment.v1.apps/k8slon-deployment \
+    k8slon="vpetersson/k8slon:$GITHASH"
